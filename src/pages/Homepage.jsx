ae7c57740c7a39eb1b207005ad61e3dda758d85a
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-13 00:13:02 +0700

    add DaisyUI

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,11 @@
 export default function Homepage() {
 
   return (
-    <div className="text-9xl text-red-500 rotate-180 text-center">
-      Test tailwindCSS
+    <div>
+      <div className="text-9xl text-red-500 rotate-180 text-center">
+        Test tailwindCSS
+      </div>
+      <button className="btn btn-primary">Test daisyUI</button>
     </div>
   );
 }
