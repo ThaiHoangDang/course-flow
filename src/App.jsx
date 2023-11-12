6abbf10079d54cbeb3caa3b279bcdde981f9817b
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-13 00:08:43 +0700

    add TailwindCSS and reorder files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,8 @@
+export default function Homepage() {
+
+  return (
+    <div className="text-9xl text-red-500 rotate-180 text-center">
+      Test tailwindCSS
+    </div>
+  );
+}
