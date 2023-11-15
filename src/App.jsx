fd499851815d35973564c609197b078e232a2bc9
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-15 11:33:36 +0700

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import Homepage from "./pages/common/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/common/Homepage";
+import Course from "./pages/common/Course";
+import CoursesList from "./pages/common/Course";
+import Login from "./pages/common/Course";
+import NotFound from "./pages/common/NotFound";
+import Profile from "./pages/common/Profile";
+import Enrolment from "./pages/students/Enrolment";
+import GradeCalculator from "./pages/students/GradeCalculator";
+import Program from "./pages/admins/Program";
+import ProgramsList from "./pages/admins/ProgramsList";
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />}/>
+          <Route exact path="/" element={<Homepage />}/>
+          <Route path="/course" element={<Course />}/>
+          <Route path="/courses-list" element={<CoursesList />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/enrolment" element={<Enrolment />}/>
+          <Route path="/grade-calculator" element={<GradeCalculator />}/>
+          <Route path="/program" element={<Program />}/>
+          <Route path="/programs-list" element={<ProgramsList />}/>
+          <Route path="*" element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
     </div>
