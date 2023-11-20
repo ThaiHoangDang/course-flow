a4ef3741f3ebb544bbdaa993f48d132e3ab46b80
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-21 02:37:30 +0700

    general design courses list page

--- src/pages/common/CoursesList.jsx
+++ src/pages/common/CoursesList.jsx
@@ -7,15 +7,72 @@ export default function CourseList() {
 	return (
 		<div>
 			<Header />
-			<div className="px-8 lg:px-32 h-32 flex justify-center items-center">
+			<div className="px-8 lg:px-32 h-32 flex justify-center items-center bg-base-200">
 				<h1 className="text-4xl">List of all courses</h1>
 			</div>
-			<div className="px-8 lg:px-32 bg-red-300 md:flex gap-5">
-				<div className="basis-4/12 bg-green-300">
-					<CoursesFilter />
+			<div className="px-8 lg:px-32 md:flex gap-5">
+				<div className="basis-4/12 my-5">
+					<div className="form-control font-bold shadow-sm border-2 px-10 py-5">
+						<p className="font-bold">Filter your results</p>
+						<input type="text" placeholder="Course code" className="input input-bordered w-full my-3" />
+						<label className="label cursor-pointer">
+							<input type="checkbox" className="checkbox" />
+							<span className="label-text">Core courses</span>
+						</label>
+						<label className="label cursor-pointer">
+							<input type="checkbox" className="checkbox" />
+							<span className="label-text">PE courses</span>
+						</label>
+						<label className="label cursor-pointer">
+							<input type="checkbox" className="checkbox" />
+							<span className="label-text">GE courses</span>
+						</label>
+					</div>
 				</div>
-				<div className="basis-8/12 bg-blue-300">
-					<Table />
+				<div className="basis-8/12 my-5 shadow-sm">
+					<div className="overflow-x-auto bg-base-100 border-2">
+						<table className="table table-zebra">
+							<thead className="shadow-sm font-bold">
+								<tr>
+									<th>Course Code</th>
+									<th>Course Name</th>
+									<th>Favorite Color</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Cy Ganderton</td>
+									<td>Quality Control Specialist</td>
+									<td>Blue</td>
+								</tr>
+								<tr>
+									<td>Hart Hagerty</td>
+									<td>Desktop Support Technician</td>
+									<td>Purple</td>
+								</tr>
+								<tr>
+									<td>Brice Swyre</td>
+									<td>Tax Accountant</td>
+									<td>Red</td>
+								</tr>
+								<tr>
+									<td>Brice Swyre</td>
+									<td>Tax Accountant</td>
+									<td>Red</td>
+								</tr>
+								<tr>
+									<td>Brice Swyre</td>
+									<td>Tax Accountant</td>
+									<td>Red</td>
+								</tr>
+								<tr>
+									<td>Brice Swyre</td>
+									<td>Tax Accountant</td>
+									<td>Red</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 			<Footer />
