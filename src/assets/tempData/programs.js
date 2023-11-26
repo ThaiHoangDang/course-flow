0aaff44eb9ad4f3166ed87373dfb4c74b75bf960
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-11-26 21:21:04 +0700

    add temp data

---
+++ src/assets/tempData/programs.js
@@ -0,0 +1,60 @@
+const programs = 
+  [
+    {
+      "code": "BP162",
+      "description": "Develop the cutting-edge industry knowledge and practical experience essential for a successful career in the IT industry.  RMIT's Bachelor of Information Technology will provide you with the knowledge to solve, support, analyse, troubleshoot and design everything from websites to business applications to programming networks for a wide range of public and private organisations.  Design your own career pathway, with the opportunity to complete a major in cyber security, enterprise system development or digital innovation.   There are 15 IT specialisations (minors) available through RMIT's IT degree, each offering industry-relevant subjects that reflect today’s emerging digital technologies. These subjects will develop your skills in a range of areas, including cloud computing, programming, data analysis, networking, data science, AI, bioinformatics and business information systems.   Work integrated learning is embedded in this degree, enabling you to gain hands-on industry-experience and create a professional network while you study.",
+      "type": "bachelor",
+      "courses": [
+        "03Rh3RJTJlY3HyBXnMzd",
+        "4reqGiJZpgh5oMJa2t8L",
+        "5XCa0LXWPFGUystXjVKB",
+        "5YpOMOFeqZv7EH8caZ64",
+        "8qe1Vc75BUelkHwwqJm5",
+        "IAIwbku07VuPXBEBb6ti",
+        "JULNebQsllGwgMJznPHW",
+        "LdbG40rfwvt24hfUdLx1",
+        "O2dToclg8mDQ6qBm7PZK",
+        "ODFCsnCqoge34IWNw0Gl",
+        "SyumFKhMje4a0qUHNBvz",
+        "XmcK79DtxE92so1sdIgv",
+        "iPHMeK0XsjvoOkgdzf6C",
+        "lhnQ3bA4QOdXD00IVRt6",
+        "qBzvOMWR3VakrOisRH6u",
+        "qOaA4olE4tfKOCT7zMqA",
+        "v9Fbkr1wpQfAKAUIIMmz",
+        "w28xvkZhhUiWa6dbKbip",
+        "wjI8RmcI75XdY31nHBMt"
+      ],
+      "name": "Bachelor of Information Technology",
+      "ID": "95L10mtBVbrFm7RSweZf"
+    },
+    {
+      "name": "Bachelor of Communication (Professional Communication)",
+      "description": "Equip yourself to become a multi-talented communicator with cross-platform skills in the exciting and fast-growing media and communication sector.    With courses that focus on advertising and public relations (PR), this career-oriented degree will train you in creating both traditional and digital content that is informative and compelling.    Advertisers seek to build the brand of a product or service to change consumer behaviour and PR practitioners look to inform and influence members of the public to build a positive opinion of an issue or organisation.   With the communications sector increasingly in need of graduates who have a combination of skills in advertising, PR and digital methods, this program will, through up-to-date learning activities, develop your capabilities to confidently step into the workforce. ",
+      "type": "bachelor",
+      "courses": [
+        "7nr7Wnq1efhaFRF4AemE",
+        "00eSxnYXNEs0fpX4pxef",
+        "Z6CKW2KDLtsSvnDJMoaj",
+        "5nGTL9Nck5WQz9vUqowr",
+        "d4dldu0SJPnltjQYqkDG",
+        "mnlXGPeQC9ayGGfeoDw5",
+        "RnVZiw77wbJRVaEIihFx",
+        "q08hpRTWjH3P1PUdiDlN",
+        "20qK1Yzgb79lMTxh8zp1",
+        "UQlni7rN6lQCaH18wvZK",
+        "qRa8EKfcGOmS0nVzybWI",
+        "cr7H228ItipFoRpgs2nd",
+        "na0w9nKw79dNfMpabeTj",
+        "qLoG0HKw2Wq1xUUQUXqM",
+        "hSAMOwMvNNVbTEoGyjvw",
+        "dQYcozVf4vpVb6p5absv",
+        "fZaKlUsCSvAJ4fySbLx2",
+        "RsD932sPuPeiErUa9Ngn",
+        "8raYluCMODv8H4faYOJB",
+        "yr2Bpg4YQRoSKmGzubrr"
+      ],
+      "code": "BP222",
+      "ID": "mMUCuU9wv4deLc0TXTFX"
+    }
+  ]
