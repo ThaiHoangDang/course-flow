510b5e0e0909a0475decb619c3b91d15be8e02d1
Nguyen Chi Nghia <58958851+temmmy@users.noreply.github.com>
2023-11-30 07:24:30 +0700

    skeleton for login page

--- src/pages/common/Login.jsx
+++ src/pages/common/Login.jsx
@@ -2,9 +2,82 @@
 export default function Login() {
 	return (
 		<div>
-			<div className="px-8 lg:px-32">
-				<h1 className="text-9xl text-center">This is Login page</h1>
-				{/* code in here */}
+			<div className="">
+				<div className="bg-gray-100 flex justify-center items-center h-screen">
+					{/* Left: Image */}
+					<div className="w-1/2 h-screen hidden lg:block">
+						<img
+							src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat"
+							alt="Placeholder Image"
+							className="object-cover w-full h-full"
+						/>
+					</div>
+					{/* Right: Login Form */}
+					<div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+						<h1 className="text-2xl font-semibold mb-4">Login</h1>
+						<form action="#" method="POST">
+							{/* Username Input */}
+							<div className="mb-4">
+								<label htmlFor="username" className="block text-gray-600">
+									Username
+								</label>
+								<input
+									type="text"
+									id="username"
+									name="username"
+									className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+									autoComplete="off"
+								/>
+							</div>
+							{/* Password Input */}
+							<div className="mb-4">
+								<label htmlFor="password" className="block text-gray-600">
+									Password
+								</label>
+								<input
+									type="password"
+									id="password"
+									name="password"
+									className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+									autoComplete="off"
+								/>
+							</div>
+							{/* Remember Me Checkbox */}
+							<div className="mb-4 flex items-center">
+								<input
+									type="checkbox"
+									id="remember"
+									name="remember"
+									className="text-blue-500"
+								/>
+								<label htmlFor="remember" className="text-gray-600 ml-2">
+									Remember Me
+								</label>
+							</div>
+							{/* Forgot Password Link */}
+							<div className="mb-6 text-blue-500">
+								<a href="#" className="hover:underline">
+									Forgot Password?
+								</a>
+							</div>
+							{/* Login Button */}
+							<button
+								type="submit"
+								className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+							>
+								Login
+							</button>
+						</form>
+						{/* Sign up  Link */}
+						<div className="mt-6 text-blue-500 text-center">
+							<a href="#" className="hover:underline">
+								Sign up Here
+							</a>
+						</div>
+					</div>
+				</div>
+
+
 			</div>
 		</div>
 	);
