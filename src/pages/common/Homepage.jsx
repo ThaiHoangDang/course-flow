728f652e5ab4fbecdeed159c3a6acdaa449be6b5
Nguyen Chi Nghia <58958851+temmmy@users.noreply.github.com>
2023-11-30 19:32:06 +0700

    Filled content for benefits and fixed responsiveness for features section on homepage

--- src/pages/common/Homepage.jsx
+++ src/pages/common/Homepage.jsx
@@ -23,29 +23,29 @@ export default function Homepage() {
         <div className="title text-center">
           <h1 className="text-5xl font-bold">Features</h1>
           <p className="py-6">Everything you need.</p>
-          <div className="flex flex-col lg:flex-row gap-10 mb-28">
-            <div class="basis-3/12 card lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
+          <div className="flex flex-col items-center justify-center lg:flex-row gap-10 mb-28">
+            <div class="card w-8/12 lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
               <figure><img src="https://plus.unsplash.com/premium_photo-1664444320101-d277eac712d2?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Shoes" /></figure>
               <div class="card-body">
                 <h2 class="card-title">Course Search and Information</h2>
                 <p>Allows students to easily find and access information about available courses, prerequisites, and descriptions, aiding in informed decision-making.</p>
               </div>
             </div>
-            <div class="basis-3/12 card lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
+            <div class="card w-8/12 lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
               <div class="card-body">
                 <h2 class="card-title">Feedback Mechanism</h2>
                 <p>Enables students to provide valuable feedback on courses, helping administrators make improvements and adjustments based on user input.</p>
               </div>
               <figure><img src="https://images.unsplash.com/photo-1613963931023-5dc59437c8a6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Shoes" /></figure>
             </div>
-            <div class="basis-3/12 card lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
+            <div class="card w-8/12 lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
               <figure><img src="https://images.unsplash.com/photo-1579208570378-8c970854bc23?q=80&w=2444&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Shoes" /></figure>
               <div class="card-body">
                 <h2 class="card-title">Ticket Overview System</h2>
                 <p>Quick access to an overview of user support tickets, allowing administrators to prioritize and address student inquiries efficiently.</p>
               </div>
             </div>
-            <div class="basis-3/12 card lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
+            <div class="card w-8/12 lg:w-3/12 bg-base-100 shadow-xl rounded-3xl">
               <div class="card-body">
                 <h2 class="card-title">Course Management Tools</h2>
                 <p>Robust tools for adding, editing, or removing courses, providing administrators with control over the course catalog.</p>
@@ -60,27 +60,26 @@ export default function Homepage() {
           <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div class="lg:pr-8 lg:pt-4">
               <div class="lg:max-w-lg">
-                <h2 class="text-base font-semibold leading-7 text-black">Lorem, ipsum dolor.</h2>
-                <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Benefits</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                <h2 class="text-base leading-7 text-black">Unlocking Advantages.</h2>
+                <h1 class="mt-2 text-6xl font-bold text-gray-900 sm:text-4xl">Benefits</h1>
                 <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                   <div class="relative pl-9">
                     <dt class="inline font-semibold text-gray-900">
                       <svg class="absolute left-1 top-1 h-5 w-5 text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
                       </svg>
-                      Push to deploy.
+                      Efficiency and Streamlined Processes.
                     </dt>
-                    <dd class="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                    <dd class="inline"> Drastically reduces manual tasks, paperwork, and administrative efforts in the course registration process, leading to streamlined operations for both students and administrators</dd>
                   </div>
                   <div class="relative pl-9">
                     <dt class="inline font-semibold text-gray-900">
                       <svg class="absolute left-1 top-1 h-5 w-5 text-black" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
                       </svg>
-                      SSL certificates.
+                      Improved User Experience and Engagement.
                     </dt>
-                    <dd class="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</dd>
+                    <dd class="inline"> Ensures a positive and user-friendly interface, making it easy for students to navigate and engage with the system. Administrators also benefit from a smoother workflow, improving overall user satisfaction</dd>
                   </div>
                   <div class="relative pl-9">
                     <dt class="inline font-semibold text-gray-900">
@@ -88,9 +87,14 @@ export default function Homepage() {
                         <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
                         <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
                       </svg>
-                      Database backups.
+                      Data-Driven Decision-Making and Planning.
                     </dt>
-                    <dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</dd>
+                    <dd class="inline">   Empowers administrators with insightful analytics, aiding in strategic decision-making. The system's data-driven approach allows for better planning, adapting to changing academic needs, and maintaining competitiveness in the educational landscape.
+
+
+
+
+                    </dd>
                   </div>
                 </dl>
               </div>
