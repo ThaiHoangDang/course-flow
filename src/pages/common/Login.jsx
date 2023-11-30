85c09c80c18800ab77cad6f1fe75b75d9c130e9a
Nguyen Chi Nghia <58958851+temmmy@users.noreply.github.com>
2023-11-30 22:55:53 +0700

    updated login page

--- src/pages/common/Login.jsx
+++ src/pages/common/Login.jsx
@@ -4,17 +4,16 @@ export default function Login() {
 		<div>
 			<div className="">
 				<div className="bg-gray-100 flex justify-center items-center h-screen">
-					{/* Left: Image */}
 					<div className="w-1/2 h-screen hidden lg:block">
 						<img
-							src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat"
-							alt="Placeholder Image"
+							src="https://images.unsplash.com/photo-1602734845056-ba5893c37370?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 							className="object-cover w-full h-full"
+							alt="login"
 						/>
 					</div>
 					{/* Right: Login Form */}
 					<div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-						<h1 className="text-2xl font-semibold mb-4">Login</h1>
+						<h1 className="text-4xl font-semibold mb-4">Login</h1>
 						<form action="#" method="POST">
 							{/* Username Input */}
 							<div className="mb-4">
@@ -25,7 +24,7 @@ export default function Login() {
 									type="text"
 									id="username"
 									name="username"
-									className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+									className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-black"
 									autoComplete="off"
 								/>
 							</div>
@@ -38,7 +37,7 @@ export default function Login() {
 									type="password"
 									id="password"
 									name="password"
-									className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+									className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-black"
 									autoComplete="off"
 								/>
 							</div>
@@ -48,32 +47,26 @@ export default function Login() {
 									type="checkbox"
 									id="remember"
 									name="remember"
-									className="text-blue-500"
+									className="text-black"
 								/>
 								<label htmlFor="remember" className="text-gray-600 ml-2">
 									Remember Me
 								</label>
 							</div>
 							{/* Forgot Password Link */}
-							<div className="mb-6 text-blue-500">
-								<a href="#" className="hover:underline">
+							<div className="mb-6 text-black">
+								<a href="login" className="hover:underline">
 									Forgot Password?
 								</a>
 							</div>
 							{/* Login Button */}
 							<button
 								type="submit"
-								className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+								className="bg-black hover:bg-black text-white font-semibold rounded-md py-2 px-4 w-full"
 							>
 								Login
 							</button>
 						</form>
-						{/* Sign up  Link */}
-						<div className="mt-6 text-blue-500 text-center">
-							<a href="#" className="hover:underline">
-								Sign up Here
-							</a>
-						</div>
 					</div>
 				</div>
 
