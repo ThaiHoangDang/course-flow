d10b0147ac1908b2bca65ad4bf533953215aa2bc
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-12-06 20:05:34 +0700

    add firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import 'firebase/firestore';
-import 'firebase/auth';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,4 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app);
+export const db = getFirestore(app);
