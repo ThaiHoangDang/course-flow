38287bdc5a47a76b97e0de3e4da7fcebf8bfd813
KoPhaiHoang <thaihoangdang1803@gmail.com>
2023-12-13 11:28:57 +0700

    so good so handsome

---
+++ src/components/Program/ProgramCoursesTable.jsx
@@ -0,0 +1,44 @@
+import { useNavigate } from "react-router-dom";
+
+
+export default function ProgramCoursesTable({courses, filterCourses, openedOnly = false}) {
+  
+  const navigate = useNavigate();
+  const handleRowClick = (id) => {
+    navigate(`/course/${id}`);
+  }  
+
+  return (
+    <div className="overflow-x-auto bg-base-100 border-1">
+      <table className="table table-zebra">
+        <thead className="shadow-sm font-bold">
+          <tr>
+            <th>Semester</th>
+            <th>Course Code</th>
+            <th>Course Name</th>
+            <th>Credits</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses != null && courses.map((course) => {
+            if (filterCourses !== "" && (! course["code"].toLowerCase().includes(filterCourses.toLowerCase())) && (! course["name"].toLowerCase().includes(filterCourses.toLowerCase()))) return null;
+            if (openedOnly && course["status"] === false) return null;
+            return (
+              <tr className="hover:underline hover:cursor-pointer" key={course["code"]} onClick={()=> handleRowClick(course["id"])}>
+                <td>{course["semester"]}</td>
+                <td>{course["code"]}</td>
+                <td>{course["name"]}</td>
+                <td>{course["credits"]}</td>
+                <td>{course["status"] === true 
+                  ? <span className="text-green-600">Opened</span>
+                  : <span className="text-red-600">Closed</span>}
+                </td>
+              </tr>
+            ); 
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/components/StudentsTable.jsx
+++ src/components/StudentsTable.jsx
@@ -1,6 +1,8 @@
 
-export default function StudentsTable({students}) {
+export default function StudentsTable({students, filterStudents}) {
   return (
+    
+
     <div className="overflow-x-auto bg-base-100 border-1">
       <table className="table table-zebra">
         <thead className="shadow-sm font-bold">
@@ -11,13 +13,16 @@ export default function StudentsTable({students}) {
           </tr>
         </thead>
         <tbody>
-          {students.map((student) => (
-            <tr>
-              <td>{student["sid"]}</td>
-              <td>{student["name"]}</td>
-              <td>{student["programID"]}</td>
-            </tr>
-          ))}
+          {students.map((student) => {
+            if (filterStudents !== "" && (! student["sid"].toLowerCase().includes(filterStudents.toLowerCase())) && (! student["name"].toLowerCase().includes(filterStudents.toLowerCase()))) return null;
+            return (
+              <tr key={student["sid"]}>
+                <td>{student["sid"]}</td>
+                <td>{student["name"]}</td>
+                <td>{student["programID"]}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
